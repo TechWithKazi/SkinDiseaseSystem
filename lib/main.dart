e0724fd128bc5bbbe4cf3b496505bbfd199555c4
TechWithKazi <langton.kazee@gmail.com>
2024-08-29 09:20:59 +0200

    .env changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,57 @@
-import 'dart:io';
-
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:skin_detection_system/mobile_phone_ui/splashScreen.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:io' show Platform; // Import Platform from dart:io
 
-Future main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-        options: const FirebaseOptions(
-            apiKey: "AIzaSyCmnSZRmoDXgPFhrnQySClEBwj4dKbMurA",
-            appId: "1:683724291000:web:fdcbc72c15e4a93d182d4d",
-            messagingSenderId: "683724291000",
-            projectId: "skin-disease-detection-s-57573"));
-  } else if (Platform.isAndroid) {
-    await Firebase.initializeApp(
-        options: const FirebaseOptions(
-            apiKey: "AIzaSyC-wyIWXjEnGPFPyMLvpir_JrtoMykwqKg",
-            appId: "1:683724291000:android:c92f93f4f82f7afb182d4d",
-            messagingSenderId: "683724291000",
-            projectId: "skin-disease-detection-s-57573",
-            storageBucket: "skin-disease-detection-s-57573.appspot.com"));
-  } else if (Platform.isIOS) {
-    await Firebase.initializeApp(
-        options: const FirebaseOptions(
-            apiKey: "AIzaSyAq4I1Rbl1cjSvNlCk5CjVAacw46UeQZiY",
-            appId: "1:683724291000:ios:67b9a0c7d75d3c02182d4d",
-            messagingSenderId: "683724291000",
-            projectId: "skin-disease-detection-s-57573",
-            storageBucket: "skin-disease-detection-s-57573.appspot.com"));
-  }
+  // Load environment variables
+  await dotenv.load();
+
+  await initializeFirebase();
 
   runApp(const MyApp());
+}
+
+Future<void> initializeFirebase() async {
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_API_KEY_WEB']!,
+        appId: dotenv.env['FIREBASE_APP_ID_WEB']!,
+        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID_WEB']!,
+        projectId: dotenv.env['FIREBASE_PROJECT_ID_WEB']!,
+      ),
+    );
+  } else if (Platform.isAndroid) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_API_KEY_ANDROID']!,
+        appId: dotenv.env['FIREBASE_APP_ID_ANDROID']!,
+        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID_ANDROID']!,
+        projectId: dotenv.env['FIREBASE_PROJECT_ID_ANDROID']!,
+        storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET_ANDROID']!,
+      ),
+    );
+  } else if (Platform.isIOS) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_API_KEY_IOS']!,
+        appId: dotenv.env['FIREBASE_APP_ID_IOS']!,
+        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID_IOS']!,
+        projectId: dotenv.env['FIREBASE_PROJECT_ID_IOS']!,
+        storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET_IOS']!,
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
